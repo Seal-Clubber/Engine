d3da132b1870d2758d3503798dc5554ae25fb8e2
Jordan Miller <Jordan.kay@gmail.com>
2024-10-03 00:05:14 -0600

    update

--- satoriengine/framework/engine.py
+++ satoriengine/framework/engine.py
@@ -6,16 +6,26 @@
 #           heavy model (pilot)
 #       provide predictions on demand
 
-from satorilib.api.disk.filetypes.csv import CSVManager
+# Engine communication ZeroMQ ? or BehaviorSubjects?
+
+# Engine training process one stream:
+
+# from satorilib.api.disk.filetypes.csv import CSVManager # df = CSVManager.read(filePath=path)
 from satorilib.api.hash import generatePathId
 from satorilib.concepts import StreamId
 from satoriengine.framework.determine_feature_set import determine_feature_set
 from satoriengine.framework.model_creation import model_create_train_test_and_predict
 from satoriengine.framework.process import process_data
+from satoriengine.framework.demonstration import demonstration
 
 streamId = StreamId(source='test', stream='test', target='test', author='test')
 path = f'./data/{generatePathId(streamId=streamId)}/aggregate.csv'
 processedData = process_data(filename=path)
 features = determine_feature_set(processedData)
 model = model_create_train_test_and_predict(features)
-# df = CSVManager.read(filePath=path)
+quickstart = demonstration(...)
+stable = quickstart
+# for loop:
+#    heavymodel = demonstration(...)
+#    # compare to stable
+#    #   replace if better
