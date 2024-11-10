453aff110c5ece11c27754ea5fff2d8f75561eaa
Jordan Miller <Jordan.kay@gmail.com>
2024-11-09 17:40:02 -0700

    getting meta to match main

--- satoriengine/framework/__init__.py
+++ satoriengine/framework/__init__.py
@@ -1,3 +1,5 @@
-# from . import engine
-
-from satoriengine.framework import engine
+# engine
+#   fit
+#       process_data -> determine_feature_set -> model_create_train_test_and_predict
+#   predit...
+#       predict?
