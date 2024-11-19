7d347f717f7d168f0c6e3be697b37a585fa63c73
krizhnaa <krizhnaprem@gmail.com>
2024-11-19 21:31:21 +0530

    minor change

--- satoriengine/framework/pipelines/__init__.py
+++ satoriengine/framework/pipelines/__init__.py
@@ -1,3 +1,3 @@
-from .interface import PipelineInterface, TrainingResult
-from .sk import SKPipeline
-from .starter import StarterPipeline
+from satoriengine.framework.pipelines.interface import PipelineInterface, TrainingResult
+from satoriengine.framework.pipelines.sk import SKPipeline
+from satoriengine.framework.pipelines.starter import StarterPipeline
