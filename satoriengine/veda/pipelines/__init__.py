451da7e39f48c8caae0ec6e9e182541d3e434b79
Jordan Miller <Jordan.kay@gmail.com>
2024-12-13 18:13:10 -0700

    wip on meta models

--- satoriengine/veda/pipelines/__init__.py
+++ satoriengine/veda/pipelines/__init__.py
@@ -1,5 +1,5 @@
 from satoriengine.veda.pipelines.interface import PipelineInterface, TrainingResult
-from satoriengine.veda.pipelines.sk import SKPipeline
+from satoriengine.veda.pipelines.sktime.sk import SKPipeline
 from satoriengine.veda.pipelines.starter import StarterPipeline
 from satoriengine.veda.pipelines.xgb import XgbPipeline
 from satoriengine.veda.pipelines.chronos_adapter import ChronosVedaPipeline
