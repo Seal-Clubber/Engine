2cd4fe60f3ca35f3e501faa0ecae0a9de8a7b730
Jordan Miller <Jordan.kay@gmail.com>
2024-12-25 09:39:04 -0700

    fix

--- satoriengine/veda/pipelines/sktime/sk.py
+++ satoriengine/veda/pipelines/sktime/sk.py
@@ -8,9 +8,9 @@ import random
 from typing import Union, Optional, List, Any
 from satorilib.logging import info, debug, error, warning, setup, DEBUG
 
-from satoriengine.veda.process import process_data
-from satoriengine.veda.determine_features import determine_feature_set
-from satoriengine.veda.model_creation import model_create_train_test_and_predict
+from satoriengine.veda.pipelines.sktime.process import process_data
+from satoriengine.veda.pipelines.sktime.determine_features import determine_feature_set
+from satoriengine.veda.pipelines.sktime.model_creation import model_create_train_test_and_predict
 from satoriengine.veda.pipelines.interface import PipelineInterface, TrainingResult
 
 setup(level=DEBUG)
