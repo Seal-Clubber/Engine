2169630f8f5247db19e1e8f305f1c401bfbf7b4c
Krishna Prem <86138069+krizhnaa@users.noreply.github.com>
2025-01-21 08:57:14 +0530

    mase calc with updated dataset (#21)

--- satoriengine/veda/adapters/xgbchronos/xgb_chronos.py
+++ satoriengine/veda/adapters/xgbchronos/xgb_chronos.py
@@ -116,7 +116,7 @@ class XgbChronosAdapter(ModelAdapter):
         if not isinstance(other, self.__class__):
             return True
         thisScore = self.score()
-        otherScore = other.modelError or other.score()
+        otherScore = other.score(test_x=self.testX, test_y=self.testY)
         isImproved = thisScore < otherScore
         if isImproved:
             info(
@@ -132,11 +132,14 @@ class XgbChronosAdapter(ModelAdapter):
             self._update(other)
         return isImproved
 
-    def score(self, **kwargs) -> float:
-        """will score the model"""
+    def score(self, test_x=None, test_y=None, **kwargs) -> float:
+        """ Will score the model """
         if self.model is None:
             return np.inf
-        self.modelError = mean_absolute_error(self.testY, self.model.predict(self.testX))
+        self.modelError = mean_absolute_error(
+            test_y if test_y is not None else self.testY, 
+            self.model.predict(test_x if test_x is not None else self.testX)
+            )
         return self.modelError
 
     def fit(self, data: pd.DataFrame, **kwargs) -> TrainingResult:
