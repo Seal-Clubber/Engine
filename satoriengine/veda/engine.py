5ea8d3d8b325e8326f01259d0e779d50fc113b89
krizhnaa <krizhnaprem@gmail.com>
2025-01-29 07:53:54 +0530

    initiate server connection

--- satoriengine/veda/engine.py
+++ satoriengine/veda/engine.py
@@ -106,12 +106,18 @@ class Engine:
         try:
             self.dataServerIp = config.get().get('server ip', '0.0.0.0')
             self.dataClient = DataClient(self.dataServerIp)
+            await self.dataClient.sendRequest(
+                    self.dataServerIp, 
+                    method='initiate-server-connection')
             info("Successfully connected to Server Ip at :", self.dataServerIp, color="green")
         except Exception as e:
             error("Error connecting to server ip in config : ", e)
             try:
                 self.dataServerIp = self.start.server.getPublicIp().text.split()[-1] # TODO : is this correct?
                 self.dataClient = DataClient(self.dataServerIp)
+                await self.dataClient.sendRequest(
+                        self.dataServerIp, 
+                        method='initiate-server-connection')
                 info("Successfully connected to Server Ip at :", self.dataServerIp, color="green")
             except Exception as e:
                 error("Failed to find a valid Server Ip : ", e)
