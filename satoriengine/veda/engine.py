b807cbed3f2ae6b3fd8cd31d0bc4136b18fd9765
krizhnaa <krizhnaprem@gmail.com>
2025-02-02 12:34:22 +0530

    engine initialization design

--- satoriengine/veda/engine.py
+++ satoriengine/veda/engine.py
@@ -640,3 +640,16 @@ class StreamModel:
 # DS: saves it to disk, sends it to any subscribing peer (EDC) as ancillary data for models
 
 
+
+
+
+async def main():
+    engine = await Engine.create()
+    await asyncio.Event().wait()
+    #await asyncio.Future()
+    #await asyncio.sleep(10)
+    #await asyncio.create_task(client._keepAlive())
+
+
+    
+asyncio.run(main())
