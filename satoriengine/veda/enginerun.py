d17fd0074fc7d18be66bd25413c9863054dc7ce2
krizhnaa <krizhnaprem@gmail.com>
2025-02-05 09:16:33 +0530

    clean

--- satoriengine/veda/enginerun.py
+++ satoriengine/veda/enginerun.py
@@ -1,14 +1,8 @@
 import asyncio
-# from .engine import Engine 
 from satoriengine.veda.engine import Engine
 
 async def main():
     await Engine.create()
     await asyncio.Event().wait()
-    #await asyncio.Future()
-    #await asyncio.sleep(10)
-    #await asyncio.create_task(client._keepAlive())
-
-
     
 asyncio.run(main())
